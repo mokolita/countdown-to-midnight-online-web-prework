008e13144efba26b7fa0c1d5bca2aadb3a7b1662
Meg Galvez <meg@oakmaine.com>
2019-02-17 02:03:25 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
 
 def countdown
+  x = 10 
+  while x > 0 
+    puts "#{x} second(s)!"
+    x -= 1 
+  end 
+    puts "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep
+  x = 10
+  while x > 0 
+    puts "#{x} second(s)!"
+    x -= 1
+    sleep 1 
+  end
+  puts "HAPPY NEW YEAR!"
+end   
